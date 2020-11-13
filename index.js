d87f0d966724bd28fbd9e057d8d60b3a8510c9f6
Ariel <Ariel@Maddies-MacBook-Pro.local>
2020-11-13 12:41:31 -0500

    [add] added blog and about

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', function(reg, res) {
     res.send('Hello, SEI World');
 });
 
+app.get('/about', function(reg, res) {
+    res.send('This is the about page.');
+
+});
+
+
+app.get('/blog', (reg, res) => {
+    res.send('Welcome to my blog');
+});
+
 app.listen(8000);
-
-
